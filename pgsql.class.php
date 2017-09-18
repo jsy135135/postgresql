051d6a6eefe25f6b0b1988dce475d7e100b3751f
jsy135135 <732677288@qq.com>
2017-09-18 18:14:28 +0800

    单独建立示例文件 Signed-off-by: jsy135135 <732677288@qq.com>

--- pgsql.class.php
+++ pgsql.class.php
@@ -121,15 +121,3 @@ class Pgsql
     return $result;
   }
 }
-
-//验证单例是否正确
-$db1 = Pgsql::getInstance();
-$rs  = $db1->findMany("select * from student");
-// $data = array('id' => 2,'name' => 'php','subjects' => 2,);
-// $rs = $db1->add('student',$data);
-// $data = array('name' => 'itcastphp');
-$condition = array('id' => 2);
-// $rs = $db1->save('student', $data, $condition);
-// $rs = $db1->delete('student',$condition);
-echo '<pre>';
-var_dump($rs);
